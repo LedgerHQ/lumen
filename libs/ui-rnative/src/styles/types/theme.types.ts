9c058716844d85ea2a2aceaaf3cdb6d43129f69b
aammami-ledger <abla.ammami@ledger.fr>
2026-01-29 12:14:53 +0100

    refactor(typography): remove responsive display types from RN

--- libs/ui-rnative/src/styles/types/theme.types.ts
+++ libs/ui-rnative/src/styles/types/theme.types.ts
@@ -5,13 +5,24 @@ import {
 import { AddEntriesNegative } from './utility.types';
 
 /**
+ * Typography tokens without responsive display (not applicable for React Native)
+ */
+export type LumenTypographyTokens = Omit<
+  TypographyTokens,
+  | 'responsiveDisplay1'
+  | 'responsiveDisplay2'
+  | 'responsiveDisplay3'
+  | 'responsiveDisplay4'
+>;
+
+/**
  * Instead of exposing the raw theme objects, narrow, extends and transform the theme to improve experience and performances
  */
 type ThemeWithOneTypographyTokens<Theme extends LumenTheme> = Omit<
   Theme,
   'typographies' | 'spacings'
 > & {
-  typographies: TypographyTokens;
+  typographies: LumenTypographyTokens;
   spacings: AddEntriesNegative<Theme['spacings']>;
 };
 
