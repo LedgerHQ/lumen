c68ec564338175d1d25c9814e48a1b9c852350f7
zel-kass <zakary.el-idrissi-kassimi@ledger.fr>
2026-01-29 17:30:57 +0100

    refactor(AddressInput): streamline Figma connection by consolidating props into enums and enhancing example component with disabled state

--- libs/ui-react/src/lib/Components/AddressInput/AddressInput.figma.tsx
+++ libs/ui-react/src/lib/Components/AddressInput/AddressInput.figma.tsx
@@ -10,84 +10,25 @@ figma.connect(
   'https://www.figma.com/design/JxaLVMTWirCpU0rsbZ30k7?node-id=7887-67',
   {
     imports: ["import { AddressInput } from '@ledgerhq/lumen-ui-react'"],
-    variant: { 
-      'input-state': 'placeholder'
-    },
     props: {
-      placeholder: figma.string('placeholder-text'),
-    },
-    example: (props) => (
-      <AddressInput
-        placeholder={props.placeholder}
-        onQrCodeClick={openQrScanner}
-      />
-    ),
-  },
-);
-
-figma.connect(
-  AddressInput,
-  'https://www.figma.com/design/JxaLVMTWirCpU0rsbZ30k7?node-id=7887-67',
-  {
-    imports: ["import { AddressInput } from '@ledgerhq/lumen-ui-react'"],
-    variant: { 
-      'input-state': 'input-address'
-    },
-    props: {
-      value: figma.string('input-text'),
-    },
-    example: (props) => (
-      <AddressInput
-        value={props.value}
-        onQrCodeClick={openQrScanner}
-      />
-    ),
-  },
-);
-
-figma.connect(
-  AddressInput,
-  'https://www.figma.com/design/JxaLVMTWirCpU0rsbZ30k7?node-id=7887-67',
-  {
-    imports: ["import { AddressInput } from '@ledgerhq/lumen-ui-react'"],
-    variant: { 
-      state: 'error',
-      'input-state': 'placeholder'
-    },
-    props: {
-      placeholder: figma.string('placeholder-text'),
+      placeholder: figma.enum('input-state', {
+        placeholder: figma.string('placeholder-text')
+      }),
+      value: figma.enum('input-state', {
+        'input-address': figma.string('input-text')
+      }),
+      disabled: figma.enum('state', {
+        disabled: true,
+      }),
       errorMessage: figma.nestedProps('.status', {
         label: figma.string('label')
       })
     },
     example: (props) => (
       <AddressInput
-        placeholder={props.placeholder}
-        onQrCodeClick={openQrScanner}
-        errorMessage={props.errorMessage.label}
-      />
-    ),
-  },
-);
-
-figma.connect(
-  AddressInput,
-  'https://www.figma.com/design/JxaLVMTWirCpU0rsbZ30k7?node-id=7887-67',
-  {
-    imports: ["import { AddressInput } from '@ledgerhq/lumen-ui-react'"],
-    variant: { 
-      state: 'error',
-      'input-state': 'input-address'
-    },
-    props: {
-      value: figma.string('input-text'),
-      errorMessage: figma.nestedProps('.status', {
-        label: figma.string('label')
-      })
-    },
-    example: (props) => (
-      <AddressInput
+        disabled={props.disabled}
         value={props.value}
+        placeholder={props.placeholder}
         onQrCodeClick={openQrScanner}
         errorMessage={props.errorMessage.label}
       />
