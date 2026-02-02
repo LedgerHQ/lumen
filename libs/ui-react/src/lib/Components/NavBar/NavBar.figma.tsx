89c79b0e4afd7deb2c1820b5049aab7dde0a1c6f
zel-kass <zakary.el-idrissi-kassimi@ledger.fr>
2026-02-02 01:25:51 +0100

    refactor(NavBar): update Figma connection URL, enhance props for back button and coin capsule, and streamline example component

--- libs/ui-react/src/lib/Components/NavBar/NavBar.figma.tsx
+++ libs/ui-react/src/lib/Components/NavBar/NavBar.figma.tsx
@@ -7,64 +7,53 @@ import {
   NavBarTrailing,
 } from './NavBar';
 import { IconButton } from '../IconButton';
-import figma from '@figma/code-connect';
+import { CryptoIcon } from '@ledgerhq/crypto-icons';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const onBack = () => {};
+import figma from '@figma/code-connect';
+import { Settings } from '../../Symbols';
 
 figma.connect(
   NavBar,
-  'https://www.figma.com/design/JxaLVMTWirCpU0rsbZ30k7/2.-Components-Library?node-id=1426-4697',
+  'https://www.figma.com/design/JxaLVMTWirCpU0rsbZ30k7?node-id=1426-4697',
   {
     imports: [
       "import { NavBar, NavBarBackButton, NavBarTitle, NavBarTrailing } from '@ledgerhq/lumen-ui-react'",
       "import { IconButton } from '@ledgerhq/lumen-ui-react'",
-      "// import { YourIconName } from '@ledgerhq/lumen-ui-react/symbols'",
     ],
     props: {
-      showBackButton: figma.boolean('show-back-button', {
-        true: true,
-        false: false,
+      backButton: figma.boolean('show-back', {
+        true: <NavBarBackButton onClick={() => {}} />,
+        false: undefined,
       }),
       title: figma.string('title'),
-      showCoinCapsule: figma.boolean('show-coin-capsule', {
-        true: true,
-        false: false,
+      coinCapsule: figma.enum('appearance', {
+        'with-asset':
+        <NavBarCoinCapsule
+          ticker='Bitcoin'
+          icon={<CryptoIcon ledgerId="bitcoin" ticker="BTC" size="24px" />}
+        />,
       }),
-      coinTicker: figma.string('ticker'),
-      trailingIcon: figma.instance('trailing-icon'),
-      showTrailing: figma.boolean('show-trailing', {
-        true: true,
-        false: false,
-      }),
-    },
-    links: [
-      {
-        name: 'Documentation',
-        url: 'https://ldls.vercel.app/?path=/docs/navigation-navbar--docs',
-      },
-    ],
-    example: (props) => (
-      <NavBar>
-        {props.showBackButton && <NavBarBackButton onClick={onBack} />}
-        {props.showCoinCapsule ? (
-          <NavBarCoinCapsule
-            ticker={props.coinTicker}
-            icon={/* CryptoIcon component */ null}
-          />
-        ) : (
-          <NavBarTitle>{props.title}</NavBarTitle>
-        )}
-        {props.showTrailing && (
+      trailing: figma.boolean('show-trailing-icon', {
+        true:
           <NavBarTrailing>
             <IconButton
               appearance='gray'
               size='sm'
-              icon={props.trailingIcon}
+              icon={Settings}
               aria-label='Action'
             />
-          </NavBarTrailing>
-        )}
+          </NavBarTrailing>,
+        false: undefined,
+      }),
+    },
+    example: (props) => (
+      <NavBar>
+        {props.backButton}
+        {props.coinCapsule}
+        <NavBarTitle>
+          {props.title}
+        </NavBarTitle>
+        {props.trailing}
       </NavBar>
     ),
   },
