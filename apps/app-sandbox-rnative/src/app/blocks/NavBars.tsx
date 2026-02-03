f8cd3a99cc7a96d6c1716f401b9662da75ac2940
Miguel Mano <aquelemiguel@gmail.com>
2026-02-03 13:06:05 +0000

    chore: fix lint

--- apps/app-sandbox-rnative/src/app/blocks/NavBars.tsx
+++ apps/app-sandbox-rnative/src/app/blocks/NavBars.tsx
@@ -1,4 +1,4 @@
-import Icon from "@ledgerhq/crypto-icons/native";
+import Icon from '@ledgerhq/crypto-icons/native';
 import {
   Box,
   NavBar,
@@ -6,34 +6,34 @@ import {
   NavBarCoinCapsule,
   NavBarDescription,
   NavBarTitle,
-} from "@ledgerhq/lumen-ui-rnative";
+} from '@ledgerhq/lumen-ui-rnative';
 
 export const NavBars = () => {
   return (
-    <Box lx={{ gap: "s16", marginVertical: "s4" }}>
+    <Box lx={{ gap: 's16', marginVertical: 's4' }}>
       <Box
         lx={{
-          backgroundColor: "surfaceTransparent",
-          borderRadius: "lg",
+          backgroundColor: 'surfaceTransparent',
+          borderRadius: 'lg',
         }}
       >
-        <NavBar appearance="compact">
+        <NavBar appearance='compact'>
           <NavBarTitle>Portfolio</NavBarTitle>
           <NavBarDescription>Manage your positions</NavBarDescription>
           <NavBarCoinCapsule
-            ticker="BTC"
-            icon={<Icon ledgerId="bitcoin" ticker="BTC" size={24} />}
+            ticker='BTC'
+            icon={<Icon ledgerId='bitcoin' ticker='BTC' size={24} />}
           />
           <NavBarBackButton />
         </NavBar>
       </Box>
       <Box
         lx={{
-          backgroundColor: "surfaceTransparent",
-          borderRadius: "lg",
+          backgroundColor: 'surfaceTransparent',
+          borderRadius: 'lg',
         }}
       >
-        <NavBar appearance="expanded">
+        <NavBar appearance='expanded'>
           <NavBarTitle>
             Are you sure you want to continue? This transfer can't be undone
             once confirmed.
@@ -42,8 +42,8 @@ export const NavBars = () => {
             Please double-check the details before proceeding.
           </NavBarDescription>
           <NavBarCoinCapsule
-            ticker="BTC"
-            icon={<Icon ledgerId="bitcoin" ticker="BTC" size={24} />}
+            ticker='BTC'
+            icon={<Icon ledgerId='bitcoin' ticker='BTC' size={24} />}
           />
           <NavBarBackButton />
         </NavBar>
@@ -51,16 +51,16 @@ export const NavBars = () => {
 
       <Box
         lx={{
-          backgroundColor: "surfaceTransparent",
-          borderRadius: "lg",
+          backgroundColor: 'surfaceTransparent',
+          borderRadius: 'lg',
         }}
       >
-        <NavBar appearance="with-asset">
+        <NavBar appearance='with-asset'>
           <NavBarTitle>Title</NavBarTitle>
           <NavBarDescription>Description</NavBarDescription>
           <NavBarCoinCapsule
-            ticker="BTC"
-            icon={<Icon ledgerId="bitcoin" ticker="BTC" size={24} />}
+            ticker='BTC'
+            icon={<Icon ledgerId='bitcoin' ticker='BTC' size={24} />}
           />
           <NavBarBackButton />
         </NavBar>
