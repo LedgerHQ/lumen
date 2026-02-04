1ca28599fd465e972c6d1680791994ac93beb9b5
Miguel Mano <aquelemiguel@gmail.com>
2026-02-04 15:47:30 +0000

    chore: fix linter

--- apps/app-sandbox-rnative/src/app/blocks/PageIndicators.tsx
+++ apps/app-sandbox-rnative/src/app/blocks/PageIndicators.tsx
@@ -3,10 +3,10 @@ import {
   Text,
   IconButton,
   PageIndicator,
-} from "@ledgerhq/lumen-ui-rnative";
-import { useTheme } from "@ledgerhq/lumen-ui-rnative/styles";
-import { ArrowLeft, ArrowRight } from "@ledgerhq/lumen-ui-rnative/symbols";
-import { useState } from "react";
+} from '@ledgerhq/lumen-ui-rnative';
+import { useTheme } from '@ledgerhq/lumen-ui-rnative/styles';
+import { ArrowLeft, ArrowRight } from '@ledgerhq/lumen-ui-rnative/symbols';
+import { useState } from 'react';
 
 export const PageIndicators = () => {
   const [page, setPage] = useState(0);
@@ -14,34 +14,34 @@ export const PageIndicators = () => {
   const { theme } = useTheme();
 
   return (
-    <Box lx={{ gap: "s32", width: "full" }}>
-      <Box lx={{ gap: "s16", alignItems: "center" }}>
+    <Box lx={{ gap: 's32', width: 'full' }}>
+      <Box lx={{ gap: 's16', alignItems: 'center' }}>
         <Box
           lx={{
-            flexDirection: "row",
-            gap: "s16",
-            alignItems: "center",
-            justifyContent: "center",
+            flexDirection: 'row',
+            gap: 's16',
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
         >
           <IconButton
             icon={ArrowLeft}
-            size="xs"
-            accessibilityLabel="Left"
-            appearance="transparent"
+            size='xs'
+            accessibilityLabel='Left'
+            appearance='transparent'
             onPress={() => setPage((v) => Math.max(0, v - 1))}
           />
           <Text
-            lx={{ color: "base", width: "s28", textAlign: "center" }}
+            lx={{ color: 'base', width: 's28', textAlign: 'center' }}
             style={{ ...theme.typographies.heading2SemiBold }}
           >
             {page}
           </Text>
           <IconButton
             icon={ArrowRight}
-            size="xs"
-            accessibilityLabel="Right"
-            appearance="transparent"
+            size='xs'
+            accessibilityLabel='Right'
+            appearance='transparent'
             onPress={() => setPage((v) => Math.min(totalPages - 1, v + 1))}
           />
         </Box>
