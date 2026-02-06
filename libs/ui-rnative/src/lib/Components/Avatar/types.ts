0dd3b6b5c0c5a54b57bf4ef8554f6533ba0c3d87
zel-kass <zakary.el-idrissi-kassimi@ledger.fr>
2026-02-06 17:28:06 +0100

    feat(ui-rnative): add Avatar component with styles and types + export component

---
+++ libs/ui-rnative/src/lib/Components/Avatar/types.ts
@@ -0,0 +1,27 @@
+import { StyledViewProps } from '../../../styles';
+
+export type AvatarProps = {
+  /**
+   * Image source URL. When undefined or on load error, displays a fallback icon.
+   * @optional
+   */
+  src?: string;
+  /**
+   * Alternative text for the image.
+   * @optional
+   * @default avatar
+   */
+  alt?: string;
+  /**
+   * The size variant of the avatar.
+   * @optional
+   * @default md
+   */
+  size?: 'sm' | 'md';
+  /**
+   * Whether to show the notifications indicator.
+   * @optional
+   * @default false
+   */
+  showNotification?: boolean;
+} & Omit<StyledViewProps, 'children'>;
