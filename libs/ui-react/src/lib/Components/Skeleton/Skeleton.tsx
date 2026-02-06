5e6560a28b33b5f67c16cfcd49293eb6db80912d
Simon Bruneaud <simon.bruneaud@ledger.fr>
2026-02-06 11:21:34 +0100

    feat(ui-react): add Skeleton variant for Table + split a bit more the component
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

--- libs/ui-react/src/lib/Components/Skeleton/Skeleton.tsx
+++ libs/ui-react/src/lib/Components/Skeleton/Skeleton.tsx
@@ -1,23 +1,34 @@
 import { cn } from '@ledgerhq/lumen-utils-shared';
 
+import { forwardRef } from 'react';
 import type { SkeletonProps } from './types';
 
 type BaseSkeletonProps = React.ComponentProps<'div'>;
 
 /** Internal base skeleton element */
-function BaseSkeleton({ className, ...props }: BaseSkeletonProps) {
-  return (
-    <div
-      className={cn('animate-pulse rounded-md bg-muted-transparent', className)}
-      {...props}
-    />
-  );
-}
-
-function Skeleton({ className, component, ...props }: SkeletonProps) {
-  if (component === 'list-item') {
+const BaseSkeleton = forwardRef<HTMLDivElement, BaseSkeletonProps>(
+  ({ className, ...props }, ref) => {
     return (
       <div
+        {...props}
+        ref={ref}
+        data-slot='skeleton'
+        data-testid='skeleton'
+        className={cn(
+          'animate-pulse rounded-md bg-muted-transparent',
+          className,
+        )}
+      />
+    );
+  },
+);
+BaseSkeleton.displayName = 'BaseSkeleton';
+
+const ListItemSkeleton = forwardRef<HTMLDivElement, BaseSkeletonProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
         data-slot='skeleton'
         data-testid='skeleton'
         className={cn('flex w-full items-center gap-16 py-8', className)}
@@ -30,18 +41,22 @@ function Skeleton({ className, component, ...props }: SkeletonProps) {
         </div>
       </div>
     );
-  }
+  },
+);
+ListItemSkeleton.displayName = 'ListItemSkeleton';
 
-  if (component === 'tile') {
+const TileSkeleton = forwardRef<HTMLDivElement, BaseSkeletonProps>(
+  ({ className, ...props }, ref) => {
     return (
       <div
+        {...props}
+        ref={ref}
         data-slot='skeleton'
         data-testid='skeleton'
         className={cn(
           'flex w-112 flex-col items-center gap-12 rounded-md px-8 py-16',
           className,
         )}
-        {...props}
       >
         <BaseSkeleton className='size-48 shrink-0 rounded-full' />
         <div className='flex w-full flex-col items-center gap-8'>
@@ -50,16 +65,73 @@ function Skeleton({ className, component, ...props }: SkeletonProps) {
         </div>
       </div>
     );
-  }
+  },
+);
+TileSkeleton.displayName = 'TileSkeleton';
 
-  return (
-    <BaseSkeleton
-      data-slot='skeleton'
-      data-testid='skeleton'
-      className={className}
-      {...props}
-    />
-  );
-}
+const TableSkeleton = forwardRef<HTMLDivElement, BaseSkeletonProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        data-slot='skeleton'
+        data-testid='skeleton'
+        className={cn('flex w-full flex-col', className)}
+        {...props}
+      >
+        <div className='flex h-40 w-full items-center'>
+          <BaseSkeleton className='h-12 w-full' />
+        </div>
+        <div className='flex h-64 items-center gap-14 opacity-90'>
+          <BaseSkeleton className='size-40 rounded-full' />
+          <div className='grow'>
+            <BaseSkeleton className='h-12 w-11/12' />
+          </div>
+        </div>
+        <div className='flex h-64 items-center gap-14 opacity-80'>
+          <BaseSkeleton className='size-40 rounded-full' />
+          <div className='grow'>
+            <BaseSkeleton className='h-12 w-10/12' />
+          </div>
+        </div>
+        <div className='flex h-64 items-center gap-14 opacity-70'>
+          <BaseSkeleton className='size-40 rounded-full' />
+          <div className='grow'>
+            <BaseSkeleton className='h-12 w-9/12' />
+          </div>
+        </div>
+      </div>
+    );
+  },
+);
+
+const componentsMap = {
+  'list-item': ListItemSkeleton,
+  tile: TileSkeleton,
+  table: TableSkeleton,
+};
+
+const Skeleton = forwardRef<HTMLDivElement, SkeletonProps>(
+  ({ className, component, ...props }, ref) => {
+    /**
+     * Check if the component is a valid pre-built variant and return the corresponding component.
+     */
+    if (component && componentsMap[component]) {
+      const Component = componentsMap[component];
+      return <Component {...props} ref={ref} className={className} />;
+    }
+
+    return (
+      <BaseSkeleton
+        {...props}
+        ref={ref}
+        data-slot='skeleton'
+        data-testid='skeleton'
+        className={className}
+      />
+    );
+  },
+);
+Skeleton.displayName = 'Skeleton';
 
 export { Skeleton };
