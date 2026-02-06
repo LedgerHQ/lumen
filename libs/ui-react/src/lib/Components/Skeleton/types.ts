cad69f8d7f760736743386f905363a97598d4dc4
Simon Bruneaud <simon.bruneaud@ledger.fr>
2026-02-06 11:21:34 +0100

    feat(ui-react): add Skeleton variant for Table + split a bit more the component
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

--- libs/ui-react/src/lib/Components/Skeleton/types.ts
+++ libs/ui-react/src/lib/Components/Skeleton/types.ts
@@ -4,7 +4,7 @@ type SkeletonProps = React.ComponentProps<'div'> & {
    * - `list-item`: Horizontal layout with circle and two text lines
    * - `tile`: Vertical centered layout with circle and two text lines in a rounded container
    */
-  component?: 'list-item' | 'tile';
+  component?: 'list-item' | 'tile' | 'table';
 };
 
 export type { SkeletonProps };
