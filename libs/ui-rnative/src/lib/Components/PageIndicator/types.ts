04b968fe74cb1a55da9f95df2802e5bf33b40164
Miguel Mano <aquelemiguel@gmail.com>
2026-02-09 11:00:46 +0000

    docs: add jsdoc for types

--- libs/ui-rnative/src/lib/Components/PageIndicator/types.ts
+++ libs/ui-rnative/src/lib/Components/PageIndicator/types.ts
@@ -1,6 +1,12 @@
 import { BoxProps } from '../Utility';
 
 export type PageIndicatorProps = {
+  /**
+   * The currently active page index. 0-based (first page is 0, second is 1, etc.).
+   */
   currentPage: number;
+  /**
+   * The total number of pages to display.
+   */
   totalPages: number;
 } & Omit<BoxProps, 'children'>;
