439a672d476b73c5388a8171ee575698bddd09ac
zel-kass <zakary.el-idrissi-kassimi@ledger.fr>
2026-02-12 10:33:46 +0100

    refactor(ui-rnative): enhance documentation for Pulse and Spin animation types

--- libs/ui-rnative/src/lib/Animations/Pulse/types.ts
+++ libs/ui-rnative/src/lib/Animations/Pulse/types.ts
@@ -1,10 +1,17 @@
 import { ReactNode } from 'react';
 
 export type PulseProps = {
-  /** The content to animate */
+  /**
+   * The content to animate
+   */
   children: ReactNode;
-  /** Duration of one complete rotation in milliseconds */
-  duration?: number;
-  /** Whether the pulse animation should play */
+  /**
+   * Duration of one complete pulse in milliseconds
+   * @default 2000
+   */
+  duration?: 75 | 100 | 150 | 200 | 300 | 500 | 700 | 1000 | 2000;
+  /**
+   * Whether the pulse animation should play
+   */
   animate?: boolean;
 };
--- libs/ui-rnative/src/lib/Animations/Spin/types.ts
+++ libs/ui-rnative/src/lib/Animations/Spin/types.ts
@@ -1,13 +1,13 @@
 import { ReactNode } from 'react';
 
 export type SpinProps = {
-  /** 
-   * The content to animate 
+  /**
+   * The content to animate
    */
   children: ReactNode;
-  /** 
-   * Duration of one complete rotation in milliseconds 
+  /**
+   * Duration of one complete rotation in milliseconds
    * @default 1000
    */
-  duration?: 75 | 100 | 150 | 200 | 300 | 500 | 700 | 1000 | 2000
+  duration?: 75 | 100 | 150 | 200 | 300 | 500 | 700 | 1000 | 2000;
 };
