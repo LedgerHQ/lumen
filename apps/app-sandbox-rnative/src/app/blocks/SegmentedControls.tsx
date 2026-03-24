b754c23c006a9585ca908479806bdaa1d1bb45e2
Miguel Mano <aquelemiguel@gmail.com>
2026-03-24 16:40:48 +0000

    test: add new hug layout to sandbox app

--- apps/app-sandbox-rnative/src/app/blocks/SegmentedControls.tsx
+++ apps/app-sandbox-rnative/src/app/blocks/SegmentedControls.tsx
@@ -2,29 +2,53 @@ import {
   Box,
   SegmentedControl,
   SegmentedControlButton,
+  Text,
 } from '@ledgerhq/lumen-ui-rnative';
 import { Code, Eye, EyeCross } from '@ledgerhq/lumen-ui-rnative/symbols';
 import React from 'react';
 
 export const SegmentedControls = () => {
-  const [state, setState] = React.useState('preview');
-  const [stateWithIcons, setStateWithIcons] = React.useState('preview');
+  const [hugState, setHugState] = React.useState('preview');
+  const [fixedState, setFixedState] = React.useState('preview');
+  const [iconsState, setIconsState] = React.useState('preview');
 
   return (
     <Box lx={{ gap: 's24', width: 'full' }}>
+      <Text typography='body2SemiBold' lx={{ color: 'muted' }}>
+        Hug
+      </Text>
       <SegmentedControl
-        selectedValue={state}
-        onSelectedChange={setState}
-        accessibilityLabel='File view'
+        selectedValue={hugState}
+        onSelectedChange={setHugState}
+        tabLayout='hug'
+        accessibilityLabel='Hug layout'
       >
         <SegmentedControlButton value='preview'>Preview</SegmentedControlButton>
         <SegmentedControlButton value='raw'>Raw</SegmentedControlButton>
         <SegmentedControlButton value='blame'>Blame</SegmentedControlButton>
       </SegmentedControl>
 
+      <Text typography='body2SemiBold' lx={{ color: 'muted' }}>
+        Fixed
+      </Text>
       <SegmentedControl
-        selectedValue={stateWithIcons}
-        onSelectedChange={setStateWithIcons}
+        selectedValue={fixedState}
+        onSelectedChange={setFixedState}
+        tabLayout='fixed'
+        accessibilityLabel='Fixed layout'
+      >
+        <SegmentedControlButton value='preview'>Preview</SegmentedControlButton>
+        <SegmentedControlButton value='raw'>Raw</SegmentedControlButton>
+        <SegmentedControlButton value='blame'>Blame</SegmentedControlButton>
+      </SegmentedControl>
+
+      <Text typography='body2SemiBold' lx={{ color: 'muted' }}>
+        With Icons (hug)
+      </Text>
+      <SegmentedControl
+        selectedValue={iconsState}
+        onSelectedChange={setIconsState}
+        tabLayout='hug'
         accessibilityLabel='File view with icons'
       >
         <SegmentedControlButton value='preview' icon={Eye}>
