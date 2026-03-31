71ade90bdcd096437b10cd09e16db5e67217de15
Simon.b <15988848+gamegee@users.noreply.github.com>
2026-03-31 16:54:40 +0200

    Code-connect multiple libraries (#601)
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

---
+++ libs/ui-rnative/src/lib/Components/Avatar/Avatar.figma.tsx
@@ -0,0 +1,29 @@
+import figma from '@figma/code-connect';
+import { Avatar } from './Avatar';
+
+figma.connect(
+  Avatar,
+  'https://www.figma.com/design/JxaLVMTWirCpU0rsbZ30k7?node-id=11097-8',
+  {
+    imports: ["import { Avatar } from '@ledgerhq/lumen-ui-rnative'"],
+    props: {
+      size: figma.enum('size', {
+        sm: 'sm',
+        md: 'md',
+        lg: 'lg',
+      }),
+      showNotification: figma.boolean('show-notification', {
+        true: true,
+        false: false,
+      }),
+    },
+    example: (props) => (
+      <Avatar
+        src='https://example-image.com'
+        size={props.size}
+        alt="John Doe's Avatar"
+        showNotification={props.showNotification}
+      />
+    ),
+  },
+);
