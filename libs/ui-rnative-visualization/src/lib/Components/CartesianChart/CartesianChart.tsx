c27e7bcc8ac8993e2250c2c6d52c29eacd4f9cb2
zel-kass <zakary.el-idrissi-kassimi@ledger.fr>
2026-04-28 15:45:19 +0200

    feat(visualisation): first draft of the Point component

--- libs/ui-rnative-visualization/src/lib/Components/CartesianChart/CartesianChart.tsx
+++ libs/ui-rnative-visualization/src/lib/Components/CartesianChart/CartesianChart.tsx
@@ -9,9 +9,9 @@ import type { CartesianChartProps } from './types';
 
 const DEFAULT_HEIGHT = 160;
 const DEFAULT_INSET: ChartInset = {
-  top: 5,
+  top: 30,
   right: 20,
-  bottom: 0,
+  bottom: 30,
   left: 20,
 };
 
