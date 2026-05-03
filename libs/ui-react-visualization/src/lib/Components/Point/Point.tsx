49039502d76667d2e4b0aa7c3e5479e8898ffb46
zel-kass <zakary.el-idrissi-kassimi@ledger.fr>
2026-05-03 18:50:05 +0200

    refactor(visualisation): update PointLabel component to use Readonly for props

--- libs/ui-react-visualization/src/lib/Components/Point/Point.tsx
+++ libs/ui-react-visualization/src/lib/Components/Point/Point.tsx
@@ -19,7 +19,7 @@ export function PointLabel({
   textAnchor = 'middle',
   dominantBaseline = 'auto',
   ...props
-}: PointLabelProps) {
+}: Readonly<PointLabelProps>) {
   return (
     <text
       textAnchor={textAnchor}
