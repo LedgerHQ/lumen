701b73ef8d55d33ec19f35c9a76d5f1a76e0b085
Miguel Mano <aquelemiguel@gmail.com>
2026-05-07 14:10:32 +0100

    story: use props for components that already implement dotindicator/count

--- libs/ui-react/src/lib/Components/DotIndicator/DotIndicator.stories.tsx
+++ libs/ui-react/src/lib/Components/DotIndicator/DotIndicator.stories.tsx
@@ -66,9 +66,7 @@ export const WithChildren: Story = {
       <DotIndicator appearance='red'>
         <Button size='sm'>Submit</Button>
       </DotIndicator>
-      <DotIndicator appearance='red'>
-        <Avatar size='md' />
-      </DotIndicator>
+      <Avatar size='md' showNotification />
       <DotIndicator appearance='red'>
         <IconButton aria-label='Settings' icon={Settings} />
       </DotIndicator>
