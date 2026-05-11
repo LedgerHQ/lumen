fe01aaccdff48ec326899592fd4405726b207174
Miguel Mano <aquelemiguel@gmail.com>
2026-05-11 20:20:51 +0100

    chore: apply same pattern to tags

--- apps/app-sandbox-rnative/src/app/blocks/Tags.tsx
+++ apps/app-sandbox-rnative/src/app/blocks/Tags.tsx
@@ -1,23 +1,49 @@
-import { Box, Tag } from '@ledgerhq/lumen-ui-rnative';
+import type { TagProps } from '@ledgerhq/lumen-ui-rnative';
+import { Box, Tag, useStyleSheet } from '@ledgerhq/lumen-ui-rnative';
 import { ExternalLink } from '@ledgerhq/lumen-ui-rnative/symbols';
 
 export const Tags = () => {
+  const styles = useStyles();
+
+  type Appearance = NonNullable<TagProps['appearance']>;
+  const appearances: Appearance[] = [
+    'accent',
+    'accent-subtle',
+    'base',
+    'gray',
+    'success',
+    'error',
+    'warning',
+  ];
+
   return (
-    <Box lx={{ gap: 's32' }}>
-      <Box lx={{ flexDirection: 'row', flexWrap: 'wrap', gap: 's8' }}>
-        <Tag appearance='accent' label='Accent' icon={ExternalLink} />
-        <Tag appearance='accent-subtle' label='Subtle' icon={ExternalLink} />
-        <Tag appearance='base' label='Base' icon={ExternalLink} />
-        <Tag appearance='gray' label='Gray' icon={ExternalLink} />
-        <Tag appearance='success' label='Success' icon={ExternalLink} />
-        <Tag appearance='error' label='Error' icon={ExternalLink} />
-        <Tag appearance='warning' label='Warning' icon={ExternalLink} />
+    <Box style={styles.container}>
+      <Box style={styles.block}>
+        {appearances.map((app) => (
+          <Tag appearance={app} label='Accent' icon={ExternalLink} />
+        ))}
         <Tag label='Disabled' icon={ExternalLink} disabled />
       </Box>
-      <Box lx={{ flexDirection: 'row', gap: 's8' }}>
+      <Box style={styles.block}>
         <Tag appearance='base' label='Base' size='md' />
         <Tag appearance='base' label='Base' size='sm' />
       </Box>
     </Box>
   );
 };
+
+const useStyles = () =>
+  useStyleSheet(
+    (t) => ({
+      container: {
+        gap: t.spacings.s32,
+      },
+      block: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        gap: t.spacings.s8,
+      },
+    }),
+    [],
+  );
