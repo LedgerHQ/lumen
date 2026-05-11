51020edbfff0794bffebbc11a08805e5ed25f800
Miguel Mano <aquelemiguel@gmail.com>
2026-05-11 20:16:06 +0100

    chore: cleanup mediatags in sandbox

--- apps/app-sandbox-rnative/src/app/blocks/MediaTags.tsx
+++ apps/app-sandbox-rnative/src/app/blocks/MediaTags.tsx
@@ -1,59 +1,51 @@
 import CryptoIcon from '@ledgerhq/crypto-icons/native';
-import { Box, MediaTag } from '@ledgerhq/lumen-ui-rnative';
+import type { MediaTagProps } from '@ledgerhq/lumen-ui-rnative';
+import {
+  Box,
+  MediaImage,
+  MediaTag,
+  useStyleSheet,
+} from '@ledgerhq/lumen-ui-rnative';
 
 export const MediaTags = () => {
+  const styles = useStyles();
+
+  const icons = [
+    <MediaImage
+      src='https://crypto-icons.ledger.com/ETH.png'
+      alt='Ethereum'
+      size={16}
+      shape='square'
+    />,
+    <CryptoIcon ledgerId='bitcoin' ticker='BTC' size={16} />,
+  ];
+
+  type Appearance = NonNullable<MediaTagProps['appearance']>;
+  const appearances: Appearance[] = [
+    'accent',
+    'accent-subtle',
+    'base',
+    'gray',
+    'success',
+    'error',
+    'warning',
+  ];
+
   return (
-    <Box lx={{ gap: 's32' }}>
-      <Box
-        lx={{
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          alignItems: 'center',
-          gap: 's8',
-        }}
-      >
-        <MediaTag
-          appearance='accent'
-          label='Accent'
-          icon={<CryptoIcon ledgerId='bitcoin' ticker='BTC' size={16} />}
-        />
-        <MediaTag
-          appearance='accent-subtle'
-          label='Subtle'
-          icon={<CryptoIcon ledgerId='bitcoin' ticker='BTC' size={16} />}
-        />
-        <MediaTag
-          appearance='base'
-          label='Base'
-          icon={<CryptoIcon ledgerId='bitcoin' ticker='BTC' size={16} />}
-        />
-        <MediaTag
-          appearance='gray'
-          label='Gray'
-          icon={<CryptoIcon ledgerId='bitcoin' ticker='BTC' size={16} />}
-        />
-        <MediaTag
-          appearance='success'
-          label='Success'
-          icon={<CryptoIcon ledgerId='bitcoin' ticker='BTC' size={16} />}
-        />
-        <MediaTag
-          appearance='error'
-          label='Error'
-          icon={<CryptoIcon ledgerId='bitcoin' ticker='BTC' size={16} />}
-        />
-        <MediaTag
-          appearance='warning'
-          label='Warning'
-          icon={<CryptoIcon ledgerId='bitcoin' ticker='BTC' size={16} />}
-        />
-        <MediaTag
-          label='Disabled'
-          icon={<CryptoIcon ledgerId='bitcoin' ticker='BTC' size={16} />}
-          disabled
-        />
+    <Box style={styles.container}>
+      <Box style={styles.block}>
+        {appearances.map((app) => (
+          <MediaTag appearance={app} label='Accent' icon={icons[0]} />
+        ))}
+        <MediaTag label='Disabled' icon={icons[0]} disabled />
       </Box>
-      <Box lx={{ flexDirection: 'row', alignItems: 'center', gap: 's8' }}>
+      <Box style={styles.block}>
+        {appearances.map((app) => (
+          <MediaTag appearance={app} label='Accent' icon={icons[1]} />
+        ))}
+        <MediaTag label='Disabled' icon={icons[0]} disabled />
+      </Box>
+      <Box style={styles.block}>
         <MediaTag
           appearance='base'
           label='Base'
@@ -70,3 +62,19 @@ export const MediaTags = () => {
     </Box>
   );
 };
+
+const useStyles = () =>
+  useStyleSheet(
+    (t) => ({
+      container: {
+        gap: t.spacings.s32,
+      },
+      block: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        gap: t.spacings.s8,
+      },
+    }),
+    [],
+  );
