84ae5bbadafc963453e6df9669e91a57a537eb98
zel-kass <zakary.el-idrissi-kassimi@ledger.fr>
2026-05-12 18:19:35 +0200

    refactor(Scrubber): remove exports that shouldn't be exposed to consumers

--- libs/ui-rnative-visualization/src/lib/Components/Scrubber/index.ts
+++ libs/ui-rnative-visualization/src/lib/Components/Scrubber/index.ts
@@ -1,8 +1,3 @@
 export { Scrubber } from './Scrubber';
-export { ScrubberProvider } from './ScrubberProvider';
 export { useScrubberContext } from './context';
-export type {
-  ScrubberContextValue,
-  ScrubberProps,
-  ScrubberProviderProps,
-} from './types';
+export type { ScrubberProps, ScrubberContextValue } from './types';
