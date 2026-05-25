64e833d3e61983d59100f370f0c97b6917cc72af
Simon Bruneaud <simon.bruneaud@ledger.fr>
2026-05-25 14:23:45 +0200

    refactor(charts): remove reveal animation on android
    Signed-off-by: Simon Bruneaud <simon.bruneaud@ledger.fr>

--- libs/ui-rnative/src/index.ts
+++ libs/ui-rnative/src/index.ts
@@ -1,4 +1,5 @@
 export { type SupportedLocale, Languages } from './i18n';
+export { RuntimeConstants } from './lib/utils';
 export { useTheme, useStyleSheet } from './styles';
 export * from './lib/Components';
 export * from './lib/Animations';
