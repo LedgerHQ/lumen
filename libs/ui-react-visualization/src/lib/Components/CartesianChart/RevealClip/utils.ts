574dc771e9647b757bb23fdcf9b8c054d32a4f53
zel-kass <zakary.el-idrissi-kassimi@ledger.fr>
2026-06-10 17:30:31 +0200

    refactor(CartesianChart): remove unused computeDataFingerprint utility function

--- libs/ui-react-visualization/src/lib/Components/CartesianChart/RevealClip/utils.ts
+++
@@ -1,9 +0,0 @@
-import type { Series } from '../../../utils/types';
-
-export const computeDataFingerprint = ({
-  series,
-}: {
-  series: Series[];
-}): string => {
-  return series.map((s) => s.data?.join(',') ?? '').join('|');
-};
