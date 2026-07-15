603837909d99bc55acb42eae647f9fdadfafaa36
Miguel Mano <hi@mano.sh>
2026-07-15 17:14:22 +0100

    use cssvar utility from design core

--- libs/ui-react/src/lib/Components/Avatar/resolveAvatarColor.ts
+++ libs/ui-react/src/lib/Components/Avatar/resolveAvatarColor.ts
@@ -1,18 +1,18 @@
-import { type CSSVarRef } from '@ledgerhq/lumen-design-core';
+import { cssVar, type CSSVarRef } from '@ledgerhq/lumen-design-core';
 import {
   resolveAvatarColorKey,
   type AvatarColorKey,
 } from '@ledgerhq/lumen-utils-shared';
 
 const AVATAR_COLOR_TOKENS: Record<AvatarColorKey, CSSVarRef> = {
-  orange: 'var(--background-avatar-orange)',
-  green: 'var(--background-avatar-green)',
-  blue: 'var(--background-avatar-blue)',
-  purple: 'var(--background-avatar-purple)',
-  red: 'var(--background-avatar-red)',
-  yellow: 'var(--background-avatar-yellow)',
-  turquoise: 'var(--background-avatar-turquoise)',
-  pink: 'var(--background-avatar-pink)',
+  orange: cssVar('var(--background-avatar-orange)'),
+  green: cssVar('var(--background-avatar-green)'),
+  blue: cssVar('var(--background-avatar-blue)'),
+  purple: cssVar('var(--background-avatar-purple)'),
+  red: cssVar('var(--background-avatar-red)'),
+  yellow: cssVar('var(--background-avatar-yellow)'),
+  turquoise: cssVar('var(--background-avatar-turquoise)'),
+  pink: cssVar('var(--background-avatar-pink)'),
 };
 
 /**
