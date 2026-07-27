1bb989a5c84a2c445a7168f90614c1c37c302b68
aammami-ledger <abla.ammami@ledger.fr>
2026-07-27 16:24:05 +0200

    fix(DonutChart): update defaultActiveId to 'bitcoin' and adjust styling for better layout

--- libs/ui-react-visualization/src/lib/Components/DonutChart/__stories__/DonutChart.stories.tsx
+++ libs/ui-react-visualization/src/lib/Components/DonutChart/__stories__/DonutChart.stories.tsx
@@ -237,7 +237,7 @@ export const WithCenterSmall: Story = {
   render: (args) => (
     <DonutChart
       {...args}
-      defaultActiveId={null}
+      defaultActiveId='bitcoin'
       renderCenter={({ series }) => (
         <DonutChartTitle>{series.length}</DonutChartTitle>
       )}
