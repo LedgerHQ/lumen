a9bf96b1182f4a4a2cb2abc7052e7d2c6832ae5c
Miguel Mano <hi@mano.sh>
2026-08-06 15:30:51 +0100

    create hex2hsl util

---
+++ libs/design-core/src/lib/colorContrast/colorContrast.ts
@@ -0,0 +1,55 @@
+type HslValue = {
+  h: number;
+  s: number;
+  l: number;
+};
+
+function hexToHsl(hex: string): HslValue {
+  const result = /^#([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+  if (result === null) {
+    throw new Error('could not parse hex color');
+  }
+
+  const [, rHex, gHex, bHex] = result;
+
+  const r = parseInt(rHex, 16) / 255;
+  const g = parseInt(gHex, 16) / 255;
+  const b = parseInt(bHex, 16) / 255;
+
+  const max = Math.max(r, g, b);
+  const min = Math.min(r, g, b);
+
+  let h = (max + min) / 2;
+  let s = h;
+  let l = h;
+
+  if (max === min) {
+    // it's achromatic
+    return { h: 0, s: 0, l };
+  }
+
+  const d = max - min;
+  s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+  if (max === r) {
+    h = (g - b) / d + (g < b ? 6 : 0);
+  } else if (max === g) {
+    h = (b - r) / d + 2;
+  } else if (max === b) {
+    h = (r - g) / d + 4;
+  }
+  h /= 6;
+
+  h = Math.round(h * 360);
+  s = Math.round(s * 100);
+  l = Math.round(l * 100);
+
+  return { h, s, l };
+}
+
+export function resolveColorContrast(
+  color: string,
+  bgColor?: string,
+  threshold = 20,
+): string {}
