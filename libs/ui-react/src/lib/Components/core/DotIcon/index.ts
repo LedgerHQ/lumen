c81bd32b2c6138cca30d92606bb8958a343353bc
Miguel Mano <hi@mano.sh>
2026-09-08 17:37:56 +0100

    add shim

--- libs/ui-react/src/lib/Components/core/DotIcon/index.ts
+++ libs/ui-react/src/lib/Components/core/DotIcon/index.ts
@@ -1,3 +1,13 @@
 export { DotIcon, dotIconSizeMap } from './DotIcon';
 export * from './getDotIconProps';
 export * from './types';
+
+// shim: @ledgerhq/crypto-icons imports this which used by our Storybook build...
+// remove once that package drops the dependency
+export const mediaImageDotSizeMap = {
+  40: 16,
+  48: 20,
+  56: 24,
+  64: 24,
+  72: 32,
+} as const;
